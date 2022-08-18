2b22be32f3f2193285bc32dc72f0f801ad83d505
Harshita Mishra <harshitamishra041@gmail.com>
2022-08-18 17:10:27 +0530

    initial commit

---
+++ src/main/java/com/susgain/util/Constants.java
@@ -0,0 +1,10 @@
+package com.susgain.util;
+
+public class Constants {
+	
+	public static String ERROR_MSG_GOAL_COST = "cost can not zero";
+	public static String ERROR_MSG_GOAL_NAME = "name can not empty";
+	
+	private Constants() {}
+
+}
